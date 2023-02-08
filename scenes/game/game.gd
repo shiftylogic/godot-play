718bfd5cf4ccaa087b8215df162ed5e9dd4be0e4
Robert Anderson <github@shiftylogic.com>
2023-02-08 15:02:50 -0800

    Simple intro + loading screens

--- scenes/game/game.gd
+++ scenes/game/game.gd
@@ -34,6 +34,11 @@ extends Node
 # Enums & Constants
 #
 
+const MINIMUM_INTRO_DELAY: float = 2.0      # seconds
+const MINIMUM_LOADING_DELAY: float = 1.0    # seconds
+
+const LoadingScene := preload( "./loading/loading.tscn" )
+
 
 #########################################
 #
@@ -46,6 +51,11 @@ extends Node
 # Private variables
 #
 
+@onready var _loader = preload( "./loader.gd" ).new()
+
+var _active: Node
+
+
 
 #########################################
 #
@@ -53,7 +63,19 @@ extends Node
 #
 
 func _ready():
-    pass
+    add_child( _loader )
+    _loader.connect( "resource_error", _report_error_terminate )
+    _loader.connect( "resource_ready", _swap_to_scene_from_intro, CONNECT_ONE_SHOT )
+
+    # On game startup, the "intro" screen is running while we prepare the
+    # main menu scene. We will then swap out the "intro" with a generic
+    # loading screen for future scene changes.
+    _active = $Main/Intro
+    _loader.load_resource( "res://scenes/main-menu/main-menu.tscn" )
+
+    # DELETE ME
+    await get_tree().create_timer( 7.0 ).timeout
+    change_scene( "res://scenes/hud/hud.tscn" )
 
 
 #########################################
@@ -61,11 +83,17 @@ func _ready():
 # Public methods
 #
 
+func change_scene( scene: String ) -> void:
+    # Remove any existing scene that is rendering
+    if _active != null:
+        _active.queue_free()
 
-#########################################
-#
-# Private methods
-#
+    # Swap to loading screen
+    _active = LoadingScene.instantiate()
+    $Main.add_child( _active )
+
+    # Start the next scene / resource loading
+    _loader.load_resource( scene )
 
 
 #########################################
@@ -73,4 +101,32 @@ func _ready():
 # Event handlers
 #
 
+func _report_error_terminate( err: String ) -> void:
+    print_debug( "An error occurred (%s). Terminating..." % err )
+    get_tree().quit( -1 )
 
+
+func _swap_to_scene( scene: PackedScene, dt: float, min_delay: float ) -> void:
+    # Allow the "intro" to stay up for at least a couple seconds
+    var delay: float = min_delay - dt
+    if delay > 0.0:
+        print_debug( "Delaying extra %.3f seconds" % delay )
+        await get_tree().create_timer( delay ).timeout
+
+    # Remove the "active" scene (which should be the loading UI
+    _active.queue_free()
+
+    # Instantiate the new scene and display it.
+    _active = scene.instantiate()
+    $Main.add_child( _active )
+
+
+func _swap_to_scene_from_intro( scene: PackedScene, dt: float ) -> void:
+    # Swap to the requested scene
+    # This should happen *before* replacing the "intro".
+    _swap_to_scene( scene, dt, MINIMUM_INTRO_DELAY )
+
+    # The "intro" screen is meant to only show once (at load time).
+    # Swap out to the generic "loading" scene for future scene changes
+    # _loader = preload( "res://scenes/game/loading/loading.tscn" ).instantiate()
+    _loader.connect( "resource_ready", _swap_to_scene.bind( MINIMUM_LOADING_DELAY ) )
---
+++ scenes/game/loader.gd
@@ -0,0 +1,106 @@
+# MIT License
+#
+# Copyright (c) 2023 Robert Anderson
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in all
+# copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+# SOFTWARE.
+
+extends Node
+
+
+#########################################
+#
+# Signals
+#
+
+signal resource_error( err )
+signal resource_started()
+signal resource_progress( progress )
+signal resource_ready( resource )
+
+
+#########################################
+#
+# Constants
+#
+
+const ERROR_INVALID_STATE = "state of loader is invalid"
+const ERROR_RESOURCE_NOT_EXIST = "requested resource does not exist"
+const ERROR_RESOURCE_REQUEST_FAILED = "requested resource failed to queue"
+const ERROR_RESOURCE_LOAD_FAILED = "requested resource failed to load"
+
+
+#########################################
+#
+# Private variables
+#
+
+var _resource: String = ""
+var _start: int
+
+
+#########################################
+#
+# Overrides (_init, _ready, others)
+#
+
+func _process( _delta: float ) -> void:
+    if _resource.is_empty():
+        return
+
+    var progress: Array[float] = []
+    match ResourceLoader.load_threaded_get_status( _resource, progress ):
+        ResourceLoader.THREAD_LOAD_INVALID_RESOURCE:
+            print_debug( "Load status (invalid resource) [\"%s\"]" % _resource )
+            resource_error.emit( ERROR_INVALID_STATE )
+            _resource = ""
+        ResourceLoader.THREAD_LOAD_IN_PROGRESS:
+            print_debug( "Load status (progress) [\"%s\"]" % _resource )
+            resource_progress.emit( progress[0] )
+        ResourceLoader.THREAD_LOAD_FAILED:
+            print_debug( "Load status (resource load failed) [\"{}\"]" % _resource )
+            resource_error.emit( ERROR_RESOURCE_LOAD_FAILED )
+            _resource = ""
+        ResourceLoader.THREAD_LOAD_LOADED:
+            print_debug( "Load status (complete) [\"%s\"]" % _resource )
+            var resource = ResourceLoader.load_threaded_get( _resource )
+            var dt: float = ( Time.get_ticks_msec() - _start ) / 1000.0
+            resource_ready.emit( resource, dt )
+            _resource = ""
+
+
+#########################################
+#
+# Public methods
+#
+
+func load_resource( resource: String ) -> void:
+    if not ResourceLoader.exists( resource ):
+        print_debug( "Requested resource (\"%s\") does not exist." % resource )
+        resource_error.emit( ERROR_RESOURCE_NOT_EXIST )
+        return
+
+    _start = Time.get_ticks_msec()
+    var err = ResourceLoader.load_threaded_request( resource )
+    if err != OK:
+        print_debug( "Requested resource (\"%s\") failed to queue." % resource )
+        resource_error.emit( ERROR_RESOURCE_REQUEST_FAILED )
+        return
+
+    _resource = resource
+    resource_started.emit()
