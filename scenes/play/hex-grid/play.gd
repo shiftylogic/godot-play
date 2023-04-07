0821f34bdedde8cb07b08f149b673e4c79e8d09e
Robert Anderson <github@shiftylogic.com>
2023-04-06 17:25:46 -0700

    Spiral hex grid generation

--- scenes/play/hex-grid/play.gd
+++ scenes/play/hex-grid/play.gd
@@ -43,20 +43,58 @@ const _Hex := preload( "./tile.tscn" )
 #
 
 const _TILE_SIZE := 1.0
+const _TILE_SHIFT := _TILE_SIZE * cos( deg_to_rad( 30 ) )
 
+const _SIDE_V := [
+    Vector4(  0.0,  1.0, -0.5,  1.0 ),      # bottom
+    Vector4(  1.0,  0.5, -1.0,  0.0 ),      # bottom-right
+    Vector4(  1.0, -0.5, -0.5, -1.0 ),      # top-right
+    Vector4(  0.0, -1.0,  0.5, -1.0 ),      # top
+    Vector4( -1.0, -0.5,  1.0,  0.0 ),      # top-left
+    Vector4( -1.0,  0.5,  0.5,  1.0 ),      # bottom-left
+]
+
+enum GenerationMode {
+    GRID,
+    SPIRAL,
+}
 
 #########################################
 #
 # Exported properties
 #
 
-@export_range( 2, 20, 1 ) var grid_size := 10
+@export
+var generation_mode: GenerationMode = GenerationMode.GRID:
+        set( v ):
+            generation_mode = v
+            _reset_grid()
+        get:
+            return generation_mode
+
+@export_range( 2, 20, 1 )
+var grid_size: int = 7:
+        set( v ):
+            grid_size = v
+            _reset_grid()
+        get:
+            return grid_size
+
+@export_range( 2, 20, 1 )
+var ring_count: int = 6:
+        set( v ):
+            ring_count = v
+            _reset_grid()
+        get:
+            return ring_count
 
 
 #########################################
 #
 # Private variables
 #
+
+@onready var _grid: Node3D = $Grid
 
 
 #########################################
@@ -65,17 +103,19 @@ const _TILE_SIZE := 1.0
 #
 
 func _ready():
-    _generate_grid()
+    _reset_grid()
 
 
-func _physics_process( _dt: float ):
-    pass
+func _unhandled_input( _ev: InputEvent ):
+    if Input.is_key_pressed( KEY_G ):
+        generation_mode = GenerationMode.GRID
+    if Input.is_key_pressed( KEY_S ):
+        generation_mode = GenerationMode.SPIRAL
 
-
-#########################################
-#
-# Public methods
-#
+    if Input.is_key_pressed( KEY_UP ):
+        _bump_size( 1 )
+    if Input.is_key_pressed( KEY_DOWN ):
+        _bump_size( -1 )
 
 
 #########################################
@@ -83,22 +123,55 @@ func _physics_process( _dt: float ):
 # Private methods
 #
 
-func _generate_grid():
-    for x in range( grid_size ):
-        var p := Vector2.ZERO
-        p.x = x * _TILE_SIZE * cos( deg_to_rad( 30 ) )
-        p.y = 0.0 if x % 2 == 0 else _TILE_SIZE / 2.0
-
-        for y in range( grid_size ):
-            var tile: Node3D = _Hex.instantiate()
-            tile.translate( Vector3( p.x, 0, p.y ) )
-            add_child( tile )
-            p.y += _TILE_SIZE
+func _bump_size( c: int ):
+    match generation_mode:
+        GenerationMode.GRID:
+            grid_size += c
+        GenerationMode.SPIRAL:
+            ring_count += c
 
 
-#########################################
-#
-# Event handlers
-#
+func _reset_grid():
+    for child in _grid.get_children():
+        child.queue_free()
+
+    match generation_mode:
+        GenerationMode.GRID:
+            _generate_grid.call_deferred( grid_size )
+        GenerationMode.SPIRAL:
+            _generate_spiral_grid.call_deferred( ring_count )
 
 
+func _generate_grid( size: int ):
+    var offset := ( size * _TILE_SIZE ) / 2.0
+    for x in range( size ):
+        var px = x * _TILE_SIZE * cos( deg_to_rad( 30 ) )
+        var py = 0.0 if x % 2 == 0 else _TILE_SIZE / 2.0
+
+        for y in range( size ):
+            _make_tile( px - offset, py - offset )
+            py += _TILE_SIZE
+
+
+func _generate_spiral_grid( rings: int ):
+    _make_tile( 0.0, 0.0 )      # Origin (middle) tile
+    for r in range( 1, rings ):
+        _generate_layer_n( r )
+
+
+func _generate_layer_n( n: int ):
+    var angle_shift := float( n ) * _TILE_SHIFT
+    var tile_shift := float( n ) * _TILE_SIZE
+
+    for side in _SIDE_V:
+        for c in range( n ):
+            _make_tile(
+                side.x * angle_shift + c * _TILE_SHIFT * side.w,
+                side.y * tile_shift  + c * _TILE_SIZE * side.z,
+            )
+
+
+func _make_tile( x: float, y: float ):
+    var tile: Node3D = _Hex.instantiate()
+    tile.translate( Vector3( x, 0, y ) )
+    _grid.add_child( tile )
